b0a28f4004611ad4681d1215bb749ac631ad02c3
Jacob Shin <jacobshin313@gmail.com>
2020-03-23 17:54:58 -0400

    Add brown

---
+++ brown/loginportal.js
@@ -0,0 +1,68 @@
+
+// When DOM is loaded
+document.addEventListener('DOMContentLoaded', function () {
+	animateUsername(animatePassword);
+	
+	
+});
+
+function animateUsername(callback) {
+	var str = ""
+	
+
+	var str =  "JohnDoe@example.com";
+    var i = 0, text; 
+
+    (function type() {
+        if (i == str.length) 
+        {
+            return;
+        }
+
+        text = str.slice(0, ++i);
+        
+		var elem = document.getElementById("email");
+        elem.focus();
+        elem.select();
+		elem.setAttribute("value", text);
+		moveCursorToEnd(elem);
+		setTimeout(type, 500);
+    }());
+    setTimeout(callback, 3400);
+}
+
+function animatePassword() {
+	var str = ""
+	
+
+	var str =  "password";
+    var i = 0, text; 
+
+    (function type() {
+        if (i == str.length) 
+        {
+            return;
+        }
+
+        text = str.slice(0, ++i);
+        
+		var elem = document.getElementById("password");
+		elem.focus();
+		elem.select();
+		elem.setAttribute("value", text);
+		moveCursorToEnd(elem);
+        setTimeout(type, 100);
+    }());
+}
+
+// https://css-tricks.com/snippets/javascript/move-cursor-to-end-of-input/
+function moveCursorToEnd(el) {
+    if (typeof el.selectionStart == "number") {
+        el.selectionStart = el.selectionEnd = el.value.length;
+    } else if (typeof el.createTextRange != "undefined") {
+        el.focus();
+        var range = el.createTextRange();
+        range.collapse(false);
+        range.select();
+    }
+}
